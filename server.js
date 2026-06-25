301b460e2e88d2acfc8b145844bf5152ee5e98a9
Y <patipraveen01@gmail.com>
2026-06-25 10:55:23 +0530

    done

--- server.js
+++ server.js
@@ -4,13 +4,28 @@ const UserData = require('./model');
 
 const app = express();
 
+app.use(express.json());
+
 mongoose.connect("")
 .then(()=>console.log("databadse connected ....")).catch((err)=>console.log(err.message))
 
 
 
 
+app.post('/send_data', async (req,res)=>{
+    const {username} = req.body
+    const {email} = req.body
+    const {Password}= req.body
+    try{
+        const newData = new UserData({username,email,Password})
+        await newData.save()
+        return res.json({"message":"Data sended"})
+    }
+    catch(err){
+        console.log(err.message)
+    }
 
+})
 
 
 
